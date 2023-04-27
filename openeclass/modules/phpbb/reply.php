9ed1c0eeaf1335618b76848d52dc9840a1e200de
jasongiotis <sdi1900041@di.uoa.gr>
2023-04-27 20:41:52 +0300

    file protection

--- openeclass/modules/phpbb/reply.php
+++ openeclass/modules/phpbb/reply.php
@@ -114,7 +114,7 @@ if (!does_exists($forum, $currentCourseID, "forum") || !does_exists($topic, $cur
 
 if (isset($submit) && $submit) {
 	checkToken();
-	// $message = htmlspecialchars($message);
+	$message = htmlspecialchars($message);
 	if (trim($message) == '') {
 		$tool_content .= $langEmptyMsg;
 		draw($tool_content, 2, 'phpbb', $head_content);
